003c0a5b0d9a3de36e9e5bde141653a6c55e525e
Alexis Pommerenke <alexis.pommerenke@dci-student.org>
2024-07-10 13:16:09 +0200

    frontend: fetch travel recommendations

--- frontend/src/views/TravelMood.jsx
+++ frontend/src/views/TravelMood.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './TravelMood.css';
 
 // Temporary arrays of objets and data that we can use to build the TravelMood component 
-const emotionsData = {
+/* const emotionsData = {
   happy: { emoji: '😊', categories: ['City', 'Beach', 'Mountain/Nature'] },
   sad: { emoji: '😢', categories: ['City', 'Beach', 'Mountain/Nature'] },
   angry: { emoji: '😡', categories: ['City', 'Beach', 'Mountain/Nature'] },
@@ -114,15 +114,39 @@ const categoriesData = {
       ]
     }
   }
-};
+}; */
 
 // TravelMood component with all the logic for the travel mood selector
 
 const TravelMood = () => {
+  const [data, setData] = ([]);
   const [selectedEmotion, setSelectedEmotion] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
   const [showCategories, setShowCategories] = useState(false);
   const [showDestinations, setShowDestinations] = useState(false);
+
+  useEffect(() => {
+    handleGetRecommendations();
+  }, []);
+
+  async function handleGetRecommendations() {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API}/travel`);
+      console.log(response);
+
+      if (response.ok) {
+        const recommendations = await response.json();
+
+        setData(recommendations);
+        console.log(data);
+      } else {
+        const { error } = await response.json();
+        throw new Error(error.message);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
 
   const handleEmotionClick = (emotion) => {
     setSelectedEmotion(emotion);
@@ -147,10 +171,64 @@ const TravelMood = () => {
   };
 
   return (
+    <>
     <div className="travel-mood">
       {!showCategories && (
         <div className="emotions-input">
-          <label className='travel-tittle' htmlFor="emotions">How are you feeling today?</label>
+          <label className='travel-title' htmlFor="emotions">How are you feeling today?</label>
+          <div className="emotions-buttons">
+            {data.map(item => (
+              <button 
+                key={item.emotion} 
+                className={`emotion-button ${selectedEmotion === item.emotion ? 'selected' : ''}`} 
+                onClick={() => handleEmotionClick(item)}
+              >
+                <span className="emoji">{item.emoji}</span>
+                {item.emotion}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+      {showCategories && !showDestinations && (
+        <div className="travel-categories">
+          {selectedEmotion.categories.map(category => (
+            <div 
+              key={category}
+              className={`category ${selectedCategory === category ? 'selected' : ''}`} 
+              onClick={() => handleCategoryClick(category)}
+            >
+              <img src={category.img} alt={category} />
+              <p>{category}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      {showDestinations && (
+        <div className="destinations">
+          {selectedEmotion[selectedCategory].destinations.map(destination => (
+            <div key={destination.name} className="destination">
+              <img src={destination.img} alt={destination.name} />
+              <p>{destination.name}</p>
+              <div className="links">
+                <a href="" target="_blank" rel="noopener noreferrer">Tips</a>
+                <a href="" target="_blank" rel="noopener noreferrer">Flights</a>
+                <a href="" target="_blank" rel="noopener noreferrer">Accommodation</a>
+                <a href="" target="_blank" rel="noopener noreferrer">Guides</a>
+                <a href="" target="_blank" rel="noopener noreferrer">Things to Do</a>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+      {(showCategories || showDestinations) && (
+        <button className="back-button" onClick={handleBackClick}>Go Back</button>
+      )}
+    </div>
+    {/* <div className="travel-mood">
+      {!showCategories && (
+        <div className="emotions-input">
+          <label className='travel-title' htmlFor="emotions">How are you feeling today?</label>
           <div className="emotions-buttons">
             {Object.keys(emotionsData).map(emotion => (
               <button 
@@ -199,7 +277,8 @@ const TravelMood = () => {
       {(showCategories || showDestinations) && (
         <button className="back-button" onClick={handleBackClick}>Go Back</button>
       )}
-    </div>
+    </div> */}
+    </>
   );
 };
 
