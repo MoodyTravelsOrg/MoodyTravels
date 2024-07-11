0810f3aae11b73bac05fdc82d3a2330967078301
Alexis Pommerenke <alexis.pommerenke@dci-student.org>
2024-07-11 15:00:01 +0200

    cleaned up navigation, added page not found

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,27 @@ import {
   Route,
   Routes,
   Navigate,
-  useLocation
+  useLocation,
+  useNavigate
 } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Homepage from "./views/Homepage";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import Homepage from "./views/Homepage";
-import Navbar from './components/Navbar/Navbar';
-import TravelMood from "./views/TravelMood";
+import MoodTracker from "./components/MoodTracker/MoodTracker";
+/* import TravelMood from "./components/TravelMood/TravelMood"; */
+import PageNotFound from "./views/PageNotFound";
+
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false); //! rename to isLoggedIn?
   const [username, setUsername] = useState("");
   const [userId, setUserId] = useState(null);
   const [userImage, setUserImage] = useState(null);
+
+  // show travel component everywhere except for register and login
+  /* const location = useLocation();
+  const showTravelMood = location.pathname !== "/login" && location.pathname !== "/register"; */
 
   useEffect(() => {
     const storedUsername = localStorage.getItem('username');
@@ -31,6 +39,7 @@ const App = () => {
     }
   }, []);
 
+  //! move to login component
   const handleLogout = () => {
     setIsAuthenticated(false);
     setUsername("");
@@ -43,50 +52,20 @@ const App = () => {
 
   return (
     <Router>
-      <Navbar 
-        isAuthenticated={isAuthenticated}
-        userImage={userImage}
-        defaultProfileImage={null} // Add the default profile image here
-        username={username}
-        onLogout={handleLogout}
-      />
-      <MainRoutes 
-        isAuthenticated={isAuthenticated}
-        setUserId={setUserId}
-        setIsAuthenticated={setIsAuthenticated}
-        setUsername={setUsername}
-        setUserImage={setUserImage}
-      />
-    </Router>
-  );
-};
-
-const MainRoutes = ({ isAuthenticated, setUserId, setIsAuthenticated, setUsername }) => {
-  const location = useLocation();
-  const showTravelMood = location.pathname !== "/login" && location.pathname !== "/register";
-
-  return (
-    <>
-      <Routes>
-        <Route path="/login" element={<Login setUserId={setUserId} setIsAuthenticated={setIsAuthenticated} setUsername={setUsername} />} />
-        <Route
-          path="/register"
-          element={
-            <Register
-              setUserId={setUserId}
-              setIsAuthenticated={setIsAuthenticated}
-              setUsername={setUsername}
-            />
-          }
-        />
-        <Route
-          path="/"
-          element={<Homepage isAuthenticated={isAuthenticated} />}
-        />
-        <Route path="*" element={<Navigate to="/" />} />
+      <Routes> 
+        {/* parent route */}
+        <Route path="/" element={<Layout isAuthenticated={isAuthenticated} userImage={userImage} username={username} onLogout={handleLogout}/>}>
+          {/* child routes */}
+          <Route index element={<Homepage isAuthenticated={isAuthenticated} />} />
+          <Route path="/mood-tracker" element={<MoodTracker />}></Route>
+          <Route path="/login" element={<Login setUserId={setUserId} setIsAuthenticated={setIsAuthenticated} setUsername={setUsername} />} />
+          <Route path="/register" element={<Register setUserId={setUserId} setIsAuthenticated={setIsAuthenticated} setUsername={setUsername} />} />
+          <Route path="*" element={<PageNotFound />} /> 
+        </Route>      
       </Routes>
-      {showTravelMood && <TravelMood />}
-    </>
+      {/* show travel component conditionally: */}
+      {/* {showTravelMood && <TravelMood />} */}
+    </Router>
   );
 };
 
