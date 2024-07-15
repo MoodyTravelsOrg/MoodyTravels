136e3c1df7fa697ac19d27ee388f1b9fef7e040e
Stefano Borrelli <stefano.palomo@dci-student.org>
2024-07-15 15:03:11 +0200

    user profile component created , added to the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import MoodTracker from "./components/MoodTracker/MoodTracker";
 import TravelMood from "./components/TravelMood/TravelMood";
 import DestinationDetail from "./components/DestinationView/DestinationView";
 import PageNotFound from "./views/PageNotFound";
+import UserProfile from './components/UserProfile/UserProfile';
 
 
 // App component with all the routing logic for make posible the navigation between the different views and keeping the Navbar component always visible at the top of the page 
@@ -56,6 +57,7 @@ const App = () => {
         <Route path="/destination/:name" element={<DestinationDetail />} />
         <Route path="/login" element={<Login setUserId={setUserId} setIsAuthenticated={setIsAuthenticated} setUsername={setUsername} />} />
         <Route path="/register" element={<Register setUserId={setUserId} setIsAuthenticated={setIsAuthenticated} setUsername={setUsername} />} />
+        <Route path="/user-profile" element={<UserProfile userId={userId} setUserImage={setUserImage} setUsername={setUsername} />} />
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
